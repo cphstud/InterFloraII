0e987dda333bf5f23264f5e8739240a0de47e1cb
thor <thor@cphbusiness.dk>
2020-11-17 18:36:20 +0100

    service

---
+++ src/BuketService.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuketService {
+    public List<Buket> getAllFlowers() {
+        List<Buket> bukets = new ArrayList<>();
+        
+
+        return bukets;
+    }
+}
