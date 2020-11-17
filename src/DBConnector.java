a011f4981b66dea218a4e0cd959283302b6f9f47
thor <thor@cphbusiness.dk>
2020-11-17 18:08:05 +0100

    first

---
+++ src/DBConnector.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+    public class DBConnector {
+        private static Connection con = null;
+        private static String uName = "root";
+        private static String pWord = "root123";
+        private static String Url = "jdbc:mysql://localhost:3306/myflowers?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+        //"jdbc:myDriver:myDatabase",
+
+        private DBConnector() {
+        }
+
+        public static Connection getCon() throws SQLException {
+            if (con == null) {
+                con = DriverManager.getConnection(
+                        Url,
+                        uName,
+                        pWord);
+
+            }
+            return con;
+        }
+
+    }
